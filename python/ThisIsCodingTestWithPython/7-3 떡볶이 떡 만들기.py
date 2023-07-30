61637927045b6a96a8eb2fc7a65727afde46c386
Lee Seyong <rred5899@gmail.com>
2023-07-31 01:38:18 +0900

    practice

--- python/ThisIsCodingTestWithPython/7-3 떡볶이 떡 만들기.py
+++ python/ThisIsCodingTestWithPython/7-3 떡볶이 떡 만들기.py
@@ -3,15 +3,22 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 L = list(map(int, input().split()))
-count = min(L)
-while True:
-    result = 0
-    for x in L:
-        result += x-count
-    if result >= M:
-        break
-    count += 1
-print(count)
+start = 0
+end = max(L)
+result = 0
+while (start <= end): # while문 이진 탐색 (주어진 N, M의 범위가 너무 크기 떄문에)
+    total = 0
+    mid = (start + end) // 2
+    for l in L:
+        if l > mid:
+            total += l - mid
+    if total < M:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
 
 '''
 4 6
