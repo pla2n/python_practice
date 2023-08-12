4fa751031b2fbdf8a49efddf2eb9644ebbdddda9
Lee Seyong <rred5899@gmail.com>
2023-08-12 22:59:45 +0900

    practice

--- python/ThisIsCodingTestWithPython2/11-6 무지의 먹방 라이브.py
+++ python/ThisIsCodingTestWithPython2/11-6 무지의 먹방 라이브.py
@@ -1,21 +1,28 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42891?language=python3
 
+# 무지의 먹방 라이브
+import heapq
+
+
 food_times = [3, 1, 2]
 k = 5
 answer = -1
-count = 0
-while count < k:
-    if not food_times:
-        answer = -1
-        break
-    for i in range(len(food_times)):
-        if food_times[i] > 0:
-            food_times[i] -= 1
-            count += 1
-            if count == k:
-                answer = i
-                break
-    if 0 in food_times:
-        food_times.remove(0)
+q = []
+for i in range(len(food_times)):
+    heapq.heappush(q, (food_times[i], i+1))
 
-print(k,"초에서 네트워크 장애가 발생했습니다.", answer, "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에", answer, "번 음식부터 다시 먹기 시작하면 됩니다.");
+food = len(food_times)
+previous = 0
+
+while q:
+    t = (q[0][0] - previous) * food
+    if k >= t:
+        k -= t
+        previous, _ = heapq.heappop(q)
+        food -= 1
+    else:
+        idx = k % food
+        q.sort(key=lambda x:x[1])
+        answer = q[idx][1]
+        break
+print(answer)
